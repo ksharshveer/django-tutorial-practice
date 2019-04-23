d657d213dfe1243c9d007f39724bd0a7b38248ee
Harshveer Singh <devillordharsh@gmail.com>
2019-04-23 14:12:20 -0700

    Changed string formatting from f"{var}" to older accepted formatting type of "%s" % var Added .gitignore

--- polls/views.py
+++ polls/views.py
@@ -16,7 +16,7 @@ def detail(request, question_id):
   return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-  return HttpResponse(f"You're looking at results of question {question_id}.")
+  return HttpResponse("You're looking at the results of question %s." % question_id)
 
 def vote(request, question_id):
-  return HttpResponse(f"You're voting on question {question_id}.")
+  return HttpResponse("You're voting on question %s." % question_id)
