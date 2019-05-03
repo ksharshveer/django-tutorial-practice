92ec60cb7b02aac181b427e4b2bf024488dc9ac8
DevilLordHarsh <devillordharsh@gmail.com>
2019-05-02 17:21:44 -0700

    modified method vote in polls/views.py from dummy implementation to real

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import loader
-from .models import Question
+from .models import Choice, Question
 
 # Create your views here.
 def index(request):
@@ -19,4 +19,16 @@ def results(request, question_id):
   return HttpResponse("You're looking at the results of question %s." % question_id)
 
 def vote(request, question_id):
-  return HttpResponse("You're voting on question %s." % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    context = {
+      'question': question,
+      'error_message': "You didn't select a choice.",
+    }
+    return render(request, 'polls/detail.html', context)
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
