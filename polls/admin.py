eea66678025560b214d4c33f820fec08ceb16959
DevilLordHarsh <devillordharsh@gmail.com>
2019-05-21 15:37:07 -0700

    customized 'Question' for admin site. Now admin can add 'Choice(s)' when adding a new 'Question'.

--- polls/admin.py
+++ polls/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+  model = Choice
+  extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
+  list_display = ('question_text', 'pub_date', 'was_published_recently')
   fieldsets = [
       (None, {
           "fields": [
@@ -16,6 +21,6 @@ class QuestionAdmin(admin.ModelAdmin):
         ],
       })
   ]
+  inlines = [ChoiceInline]
   
-
 admin.site.register(Question, QuestionAdmin)
